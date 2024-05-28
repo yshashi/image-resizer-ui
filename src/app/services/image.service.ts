47f0b2a27ff17d768a7640d828727cb128ce3a05
Sashikumar <yshashi30@gmail.com>
2024-05-29 01:05:39 +0800

    refactor: Update image service base URL to use HTTPS

--- src/app/services/image.service.ts
+++ src/app/services/image.service.ts
@@ -5,7 +5,7 @@ import { Injectable, inject } from '@angular/core';
   providedIn: 'root',
 })
 export class ImageService {
-  #baseUrl = 'http://localhost:5000';
+  #baseUrl = 'https://image-resizer-1ahd.onrender.com';
   #http = inject(HttpClient);
 
   upload(formData: FormData) {
